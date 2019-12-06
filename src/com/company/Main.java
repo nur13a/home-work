b7e332e8f9ccf4307a74a1ff9a6c88e0fed53733
Администратор <nuriza.samidinovna@gmail.com>
2019-12-07 02:46:45 +0600

    home work 30

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Climbable koala=new Climbable() {
+        Climbable koala = new Climbable() {
             @Override
             public void climb() {
                 System.out.println("I'm Koala");
-            }};
-        Monkey monkey=new Monkey(koala );
+            }
+        };
+        Monkey monkey = new Monkey(koala);
     }
 }
-
